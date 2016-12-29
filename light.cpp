e33f7ac5c4c5150c1895b4d657a17a5ee577a2ac
VincentZhou98 <zhoutiangang@pku.edu.cn>
2016-12-29 23:41:00 +0800

    loading and saving
    1.点击时再生成setting and loading（指针形式）
    2.进度条可移动，且到时间后关闭（delete this）

--- light.cpp
+++ light.cpp
@@ -53,7 +53,8 @@ void Light::reset_point_at_screen(){
 //光线与平面镜的反射
 Light Light::reflect(QLineF l){
     Light light(QPointF(0,0),0);
-    if(this->line().intersect(l,nullptr)!=1) return light;
+    if(this->line().intersect(l,nullptr)!=1) return light;  //If the class deﬁnition declares a move constructor or move assignment operator,
+                                                            //the implicitly declared copy constructor is deﬁned as deleted
     else {
         QPointF p=intersect_point(l);
         this->line().setP2(p);
