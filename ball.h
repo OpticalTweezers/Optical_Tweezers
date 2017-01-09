3e3ccd2fdfce1a644302020c66355282daa43a36
astrosirius0305 <astrosirius0305@sohu.com>
2017-01-09 21:28:53 +0800

    Revert "球折射光线"
    This reverts commit ffc6bec73ba65971639ad8cf05c1f38efef041b7.

--- ball.h
+++ ball.h
@@ -7,13 +7,10 @@
 #include <QGraphicsPixmapItem>
 #include <QPointF>
 #include <QVector2D>
-#include "Light.h"
 class Ball:public QObject,public QGraphicsPixmapItem{    //bad inherited error
     Q_OBJECT
 public:
     Ball(QGraphicsItem * parent=0);
-    QPointF Ball::intersect_point(Light light);
-    void refract(Light light);
 public slots:
     void move();                 //ball移动
     void v_change(QVector2D force);         //与move配套
@@ -21,9 +18,6 @@ private:
     bool out_of_scene();   //move辅助函数
     QVector2D velocity;
     QVector2D acceleration;
-    const double radius = 35.5;
-    const double n = 1.5;
-    QPointF center;
 //protected:
     //void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget);
 };
